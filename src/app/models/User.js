c287c29985596648843bc9d7d71470240411637a
Vlamir Santo <vlamir.santo@dotlib.com.br>
2019-07-17 15:24:40 -0300

    Início da autenticação por jwt.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../../config/auth')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -22,7 +24,10 @@ const UserSchema = new mongoose.Schema({
   }
 })
 
-// Hook para criptografar a senha
+/**
+ * Hook para criptografar a senha
+ * Executa antes do método SAVE do mongoose (inclusão ou alteração)
+ */
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     return next()
@@ -30,5 +35,24 @@ UserSchema.pre('save', async function (next) {
 
   this.password = await bcrypt.hash(this.password, 8)
 })
+
+/**
+ * Adicionando métodos à instância do model
+ */
+UserSchema.methods = {
+  // Comparação de senhas criptografadas
+  compareHash (password) {
+    return bcrypt.compare(password, this.password)
+  }
+}
+
+/**
+ * Métodos estáticos disparados diretamente pelo model
+ */
+UserSchema.statics = {
+  generateToken ({ id }) {
+    return jwt.sign({ id }, authConfig.secret, { expiresIn: authConfig.ttl })
+  }
+}
 
 module.exports = mongoose.model('User', UserSchema)
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const UserController = require('./app/controllers/UserController')
+const SessionsController = require('./app/controllers/SessionController')
+
+const authMiddleware = require('./app/middlewares/auth')
 
 const routes = express.Router()
 
 routes.post('/users', UserController.save)
+routes.post('/sessions', SessionsController.save)
+routes.get('/token', authMiddleware, (req, res) => res.json({ ok: true }))
 
 module.exports = routes
