ba9c475fa886e43a822a0d5c8e2dcd860e2d8b90
Vlamir Santo <vlamir.santo@dotlib.com.br>
2019-07-19 17:08:03 -0300

    Template do email.

---
+++ src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'd30ee731f9e62a',
+    pass: 'dbab0c3cad4af4'
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -12,10 +12,19 @@ routes.post('/sessions', controllers.SessionController.save)
 routes.get('/token', authMiddleware, (req, res) => res.json({ ok: true }))
 
 routes.use(authMiddleware)
+
+/**
+ * Ads
+ */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
 routes.post('/ads', controllers.AdController.store)
 routes.put('/ads/:id', controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
+
+/**
+ * Purchases
+ */
+routes.post('/purchase', controllers.PurchaseController.save)
 
 module.exports = routes
